07ffd6f9d307df677a160fa9d3803d6487e60b51
charlie-2201 <53409083+charlie-2201@users.noreply.github.com>
2020-04-19 05:45:14 +0530

    Update index.php

--- Student/index.php
+++ Student/index.php
@@ -1,57 +1,83 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>Student Management System</title>
-	<style type="text/css">
-   .header
-   {
-   background-color:#202020;
-  color: white;
-  margin: 50px;
-  padding: 50px;
-}
- </style>
+    <title>Student Management System</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+    <style>
+    body {
+        background-image: url("unnamed.jpg");
+		background-size: cover;
+		background-repeat: no-repeat;
+    }
+    </style>
+
 </head>
-<body style="background-color:#34495e">
-	<div class="header">
-<h1 align="center">Welcome to Student Management System</h1>
-<h3 align="right" style="margin-right: 20px">
-	<a href="login.php">Admin Login</a></h3>
-</div>
 
-	<form method="post" action="index.php">
-		<table style="width:50%;" align="center" border="1">
-			<tr>
-				<td colspan="2" align="center">Student Information</td>
-			</tr>
-			<tr>
-			<td align="left">Choose Semester</td>
-			<td><Select name="sem">
-				<option value="1">1st</option>
-				<option value="2">2nd</option>
-				<option value="3">3rd</option>
-				<option value="4">4th</option>
-				<option value="5">5th</option>
-				<option value="6">6th</option>
-				<option value="7">7th</option>
-				<option value="8">8th</option>
-			</Select>
-		</td>
-		</tr>
-		<tr>
-			<td align="left">Enter USN</td>
-			<td><input type="text" name="usn" required="required"></td>
-		</tr>
-		<tr>
-			<td colspan="1" align="center"></td>
-			<td>
-				<input type="submit" name="submit" value="Show Information"></td>
+<body class="effect" style="background-color:#FBF8F0">
+    <nav class="navbar navbar-expand-lg navbar-light bg-white">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03"
+            aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="#">Student Management System</a>
+        <a 
 
-		</tr>
-		</table>
-		
-	</form>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul class="navbar-nav mr-sm-2">
+                <li class="nav-item" >
+                    <a class="nav-link" href="#">Admin</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Student</a>
+                </li>
+                </ul>
+        </div>
+    </nav><div style="margin-top: 10vw"></div>
+
+    <form method="post" action="index.php">
+        <table style="width:30%; color: black;" align="center" border="5">
+            <tr>
+                <td colspan="2" align="center">Student Information</td>
+            </tr>
+            <tr>
+                <td align="left">Choose Semester</td>
+                <td><Select name="sem">
+                        <option value="1">1st</option>
+                        <option value="2">2nd</option>
+                        <option value="3">3rd</option>
+                        <option value="4">4th</option>
+                        <option value="5">5th</option>
+                        <option value="6">6th</option>
+                        <option value="7">7th</option>
+                        <option value="8">8th</option>
+                    </Select>
+                </td>
+            </tr>
+            <tr>
+                <td align="left">Enter USN</td>
+                <td><input type="text" name="usn" required="required"></td>
+            </tr>
+            <tr>
+                <td align="center" colspan="2">
+                    <input type="submit" name="submit" value="Show Information"></td>
+
+            </tr>
+        </table>
+
+    </form>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
 <?php
 if(isset($_POST['submit']))
